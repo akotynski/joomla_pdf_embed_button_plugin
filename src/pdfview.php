6690c9bc0ea0028e598e093820d265c755fea4b1
akotynski <aleksanderkotbury@gmail.com>
2017-09-02 19:09:43 +0200

    fix typos

--- src/pdfview.php
+++ src/pdfview.php
@@ -16,7 +16,7 @@ Width: <input type="text" name="width" value="' . $width . '"><br>
 <br>
 Height: <input type="text" name="height" value="' . $height . '"><br>
 <br>
-Styl: <input type="radio" name="engine" value="native">Native <input type="radio" name="engine" value="google" checked>Google <input type="radio" name="engine" value="pdfjs">pdfjs
+Style: <input type="radio" name="engine" value="native">Native <input type="radio" name="engine" value="google" checked>Google <input type="radio" name="engine" value="pdfjs">pdfjs
 <br><br>
 <input type="submit" value="Insert PDF">
 </form>';
